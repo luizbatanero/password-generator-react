8375585e75506f17c04d55cf5eb7a55b03fb5b2f
Luiz Batanero <luizbatanero@gmail.com>
2019-05-31 14:16:21 -0300

    App styles

--- src/App.js
+++ src/App.js
@@ -2,22 +2,40 @@ import React from 'react';
 import Settings from './components/Settings';
 import PasswordList from './components/PasswordList';
 import { generatePasswords } from './utility/password-generator';
+import { Grid, Header } from 'semantic-ui-react';
+import './App.css';
 
 class App extends React.Component {
   state = {
-    passwords: []
+    passwords: [],
+    isLoading: false
   };
 
   handleGenerate = settings => {
-    this.setState({ passwords: generatePasswords(settings) });
+    this.setState({ isLoading: true }, () => {
+      setTimeout(() => {
+        this.setState({
+          passwords: generatePasswords(settings),
+          isLoading: false
+        });
+      }, 500);
+    });
   };
 
   render() {
     return (
-      <div style={{ width: 500, margin: '50px auto' }}>
-        <Settings handleGenerate={this.handleGenerate} />
-        <PasswordList passwords={this.state.passwords} />
-      </div>
+      <Grid container centered className="App">
+        <Grid.Column style={{ maxWidth: 450, padding: '70px 0' }}>
+          <Header as="h2" textAlign="center">
+            Password Generator
+          </Header>
+          <Settings
+            handleGenerate={this.handleGenerate}
+            isLoading={this.state.isLoading}
+          />
+          <PasswordList passwords={this.state.passwords} />
+        </Grid.Column>
+      </Grid>
     );
   }
 }
